03e43e9f458fa834f60498995835e3288e3e75b5
Ommunde <ommunde9529@gmail.com>
2026-03-07 15:52:31 +0530

    Cloud Storage, PDF-img conversion, AI feedback

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,8 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
+import Upload from "./routes/upload";
 
-export default [index("routes/home.tsx"), route('/auth', 'routes/auth.tsx')] satisfies RouteConfig;
+export default [
+    index("routes/home.tsx"), 
+    route('/auth', 'routes/auth.tsx'), 
+    route('/upload', 'routes/upload.tsx'),
+] satisfies RouteConfig;
